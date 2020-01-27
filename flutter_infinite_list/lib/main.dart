1fecce2d9a71a6fd85f3fa6c30ffd81e5678d7d4
Americo29 <inge.americo@gmail.com>
2020-01-27 14:12:42 -0500

    se agregó ejemplo con infinite List

---
+++ flutter_infinite_list/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_infinite_list/src/bloc/bloc.dart';
+import 'package:flutter_infinite_list/src/pages/home_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/src/bloc/post_bloc.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Infinite Scroll',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Posts'),
+        ),
+        body: BlocProvider(
+          create: (context) =>
+            PostBloc(httpClient: http.Client())..add(Fetch()),
+            child: HomePage(),
+        )
+      ),
+    );
+  }
+}
